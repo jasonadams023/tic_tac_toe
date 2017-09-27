b13a4dc092e0ab05419e70ff5eba240ed63f1119
Jason Adams <jadams@thoughtworks.com>
2017-09-27 17:23:27 -0500

    Update snapshot for cat's game gameSnapshot

--- test/__tests__/components/game.snapshot.spec.js
+++ test/__tests__/components/game.snapshot.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Game from '../../../app/components/game';
+import { generateBoard } from '../../../app/components/game-functions';
 
 describe('Game', () => {
     it('displays a new game', () => {
@@ -11,6 +12,27 @@ describe('Game', () => {
     });
 
     it('displays a cat\'s game', () => {
+        let x = 'X';
+        let o = 'O';
 
+        let finalSquares = generateBoard(3);
+        finalSquares[0][0] = x;
+        finalSquares[0][1] = o;
+        finalSquares[0][2] = x;
+        finalSquares[1][0] = x;
+        finalSquares[1][1] = o;
+        finalSquares[1][2] = x;
+        finalSquares[2][0] = o;
+        finalSquares[2][1] = x;
+        finalSquares[2][2] = o;
+
+        const wrapper = shallow (
+            < Game />
+        );
+
+        wrapper.setState( { history: [{ squares: finalSquares }] } );
+        wrapper.update();
+
+        expect(wrapper).toMatchSnapshot();
     });
 });
