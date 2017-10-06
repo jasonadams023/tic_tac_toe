f5b6f90da09273dc5ae27e0c06e305c65b6ec64d
Jason Adams <jadams@thoughtworks.com>
2017-10-06 09:55:58 -0500

    Remove old comments from Board.js

--- app/components/board.js
+++ app/components/board.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import TicTacToeTile from './tic-tac-toe-tile';
 
-
 export default function Board (props, { store }) {
     const state = store.getState();
     const history = state.history;
@@ -32,39 +31,6 @@ export default function Board (props, { store }) {
         </div>
     );
 }
-
-
-//export default class Board extends React.Component {
-//  render() {
-//    const { store } = this.context;
-//    const state = store.getState();
-//    const history = state.history;
-//    const current = history[state.stepNumber];
-//    const squares = current.squares;
-//
-//    return (
-//        <div className="game-board" >
-//            {
-//                squares.map((row, xIndex) => {
-//                    return <div key={ xIndex } className="board-row">
-//                        {
-//                            row.map((space, yIndex) => {
-//                                const coordinates = {x: xIndex, y: yIndex};
-//                                return (
-//                                    < TicTacToeTile
-//                                        key={ xIndex + '-' + yIndex }
-//                                        coordinates={ coordinates }
-//                                    />
-//                                );
-//                            })
-//                        }
-//                    </div>
-//                })
-//            }
-//        </div>
-//    );
-//  }
-//}
 
 Board.contextTypes = {
     store: PropTypes.object
