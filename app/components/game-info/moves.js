f0aa291a5420bbfaf9a64e52109d721bcb979ada
Jason Adams <jadams@thoughtworks.com>
2017-10-06 09:49:04 -0500

    Refactor Moves into functional component

--- app/components/game-info/moves.js
+++ app/components/game-info/moves.js
@@ -3,25 +3,21 @@ import PropTypes from 'prop-types';
 
 import Move from './moves';
 
-export default class Moves extends React.Component {
-    render() {
-        const { store } = this.context;
+export default function Moves (props, { store }) {
+    const handleClick = (step) => { store.dispatch({ type: "CHANGE_STEP", stepNumber: step }); };
 
-        const handleClick = (step) => { store.dispatch({ type: "CHANGE_STEP", stepNumber: step }); };
-
-        const moves = store.getState().history.map((step, move) => {
-            const description = move ? 'Move #' + move : 'Game Start';
-            return (
-                <li key={move} className="moves">
-                    <button onClick={() => handleClick(move)}>{ description }</button>
-                </li>
-           );
-        });
-
+    const moves = store.getState().history.map((step, move) => {
+        const description = move ? 'Move #' + move : 'Game Start';
         return (
-            <ol>{ moves }</ol>
-        );
-    }
+            <li key={move} className="moves">
+                <button onClick={() => handleClick(move)}>{ description }</button>
+            </li>
+       );
+    });
+
+    return (
+        <ol>{ moves }</ol>
+    );
 }
 
 Moves.contextTypes = {
